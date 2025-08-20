50b51b06b3d38ba16767e2fb71272414c9275d09
riyanhsibariya <bariyariyanshi@gmail.com>
2025-08-20 16:15:48 +0530

    Final db

--- app_settings.py
+++ app_settings.py
@@ -2,4 +2,5 @@
 class cls_app_settings:
     SECRET_KEY = "flaskfw_secret_key"
     JWT_SECRET_KEY = "flaskfw_jwt_secret_key"
-    MONGO_URI = "mongodb+srv://bariyariyanshi:<db_password>@cluster0.z3kh79n.mongodb.net/optivision"
+    MONGO_URI = "mongodb+srv://bariyariyanshi:0BzTCpkT0rLT7XkD@cluster0.z3kh79n.mongodb.net/optivision?retryWrites=true&w=majority&appName=Cluster0"
+    
