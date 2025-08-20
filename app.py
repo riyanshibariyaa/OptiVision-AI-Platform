8ec20a055a5f630411b8bbf29f83c272f205f28a
Riyanshi <168426442+riyanshibariyaa@users.noreply.github.com>
2025-08-20 21:29:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -957,5 +957,5 @@ def debug_detection():
 
 ################################################################## 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 6000))
     app.run(host='0.0.0.0', port=port, debug=False)
