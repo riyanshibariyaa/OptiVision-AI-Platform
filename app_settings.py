2933abee24d40fecf4486c23dfcd5153b7856243
Riyanshi <168426442+riyanshibariyaa@users.noreply.github.com>
2025-08-20 23:36:33 +0530

    Update app_settings.py

--- app_settings.py
+++ app_settings.py
@@ -2,5 +2,5 @@
 class cls_app_settings:
     SECRET_KEY = "flaskfw_secret_key"
     JWT_SECRET_KEY = "flaskfw_jwt_secret_key"
-    MONGO_URI = "mongodb://localhost:27017/Optivision"
+    MONGO_URI = "mongodb+srv://bariyariyanshi:0BzTCpkT0rLT7XkD@cluster0.z3kh79n.mongodb.net/optivision?retryWrites=true&w=majority&appName=Cluster0"
     
